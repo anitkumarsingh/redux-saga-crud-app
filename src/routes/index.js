3d9fec623194a5b6fd77bfb478d34b25cdb20ade
anitkumarsingh <anikumarsingh99@gmail.com>
2022-06-28 21:56:31 +0530

    header

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
 import About from '../pages/About';
 import Home from '../pages/Home';
 import UserAddEdit from '../pages/UserAddEdit';
@@ -6,23 +7,26 @@ import UserInfo from '../pages/UserInfo';
 
 const Main = () => {
 	return (
-		<Switch>
-			<Route path='/user/:id'>
-				<UserInfo />
-			</Route>
-			<Route path='/user-edit/:id'>
-				<UserAddEdit />
-			</Route>
-			<Route path='/user-add'>
-				<UserAddEdit />
-			</Route>
-			<Route path='/about'>
-				<About />
-			</Route>
-			<Route exect path='/'>
-				<Home />
-			</Route>
-		</Switch>
+		<>
+			<Header />
+			<Switch>
+				<Route path='/user/:id'>
+					<UserInfo />
+				</Route>
+				<Route path='/user-edit/:id'>
+					<UserAddEdit />
+				</Route>
+				<Route path='/user-add'>
+					<UserAddEdit />
+				</Route>
+				<Route path='/about'>
+					<About />
+				</Route>
+				<Route exect path='/'>
+					<Home />
+				</Route>
+			</Switch>
+		</>
 	);
 };
 
